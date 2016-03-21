33b99dc0261f9f668c2ab724d61ccbf406fc93a5
Linda Kristiansen <linda@visue.dk>
2016-03-21 09:49:53 +0100

    Added files via upload

--- footer.php
+++ footer.php
@@ -11,16 +11,21 @@
 
 ?>
 
-	</div><!-- #content -->
+			</div><!-- #content -->
+        </div>
+    </div>
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'visuebasic' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'visuebasic' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'visuebasic' ), 'visuebasic', '<a href="https://visue.dk" rel="designer">Linda Kristiansen</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+	<div class="footer-area full">
+    	<div class="main-page">
+            <footer id="colophon" class="site-footer inner" role="contentinfo">
+                <div class="site-info">
+                    <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'visuebasic' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'visuebasic' ), 'WordPress' ); ?></a>
+                    <span class="sep"> | </span>
+                    <?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'visuebasic' ), 'visuebasic', '<a href="https://visue.dk" rel="designer">Linda Kristiansen</a>' ); ?>
+                </div><!-- .site-info -->
+            </footer><!-- #colophon -->
+		</div>
+    </div>
 
 <?php wp_footer(); ?>
 
